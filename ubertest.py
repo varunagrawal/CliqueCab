83055232057e4b4ef9204badd5a77680a1e0b84d
Varun Agrawal <varagrawal@gmail.com>
2015-05-02 10:16:36 +0530

    Python test files

---
+++ ubertest.py
@@ -0,0 +1,12 @@
+uber_sandbox = "https://sandbox-api.uber.com/v1"
+
+##### Do in browser #####
+auth = "https://login.uber.com/oauth/authorize?response_type=token&client_id=h2CYRccylaZtoCRBJrv7OjqbUsJ9kkxR"
+
+
+# Get the access token from the result
+
+    
+
+#####---------------#####
+
